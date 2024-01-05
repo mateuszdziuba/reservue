5171197452fee04cdb423fe4349edddeb71f57bc
Mateusz Dziuba <mkdziuba@gmail.com>
2024-01-05 18:46:09 +0100

    fix: fix lockfile error

--- apps/nextjs/src/components/ui/calendar.tsx
+++ apps/nextjs/src/components/ui/calendar.tsx
@@ -5,7 +5,6 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { buttonVariants } from "~/components/ui/button";
-
 import { cn } from "~/lib/utils";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
